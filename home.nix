76d4a9781c59fdf6af90d83abcf03e1cb8cf47d9
guoe2005 <guoe2005@126.com>
2023-11-04 10:02:18 +0800

    ?

--- home.nix
+++ home.nix
@@ -168,18 +168,18 @@
   # Enable zsh
   programs.zsh = {
     enable = true;
-      oh-my-zsh = {
+    oh-my-zsh = {
       enable = true;
-      theme = "norm";
+      theme = "gentoo";
       };
-      plugins = [
+    plugins = [
       {
       name = "zsh-autosuggestions";
       src = pkgs.fetchFromGitHub {
-        owner = "zsh-users";
-        repo = "zsh-autosuggestions";
-        rev = "v0.4.0";
-        sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
+      owner = "zsh-users";
+      repo = "zsh-autosuggestions";
+      rev = "v0.4.0";
+      sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
         };
       }
      ];
