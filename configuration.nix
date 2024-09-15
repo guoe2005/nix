ceb55b09bf2931d3ae14725ca4f08b14e703edea
guoe2005 <guoe2005@126.com>
2024-09-15 11:23:37 +0800

    fcitx

--- configuration.nix
+++ configuration.nix
@@ -155,13 +155,16 @@
     # ibus.engines =  with pkgs.ibus-engines; [
     #   libpinyin
     # ];
-    fcitx5.waylandFrontend = true;
-   type = "fcitx5";
-   enable = true;
-   fcitx5.addons = with pkgs; [
-     fcitx5-mozc
-     fcitx5-gtk
-   ];
+   fcitx5.waylandFrontend = true;
+   i18n.inputMethod = {
+     enabled = "fcitx5";
+     waylandFrontend = true;
+     fcitx5.addons = with pkgs; [
+       fcitx5-gtk             # alternatively, kdePackages.fcitx5-qt
+       fcitx5-chinese-addons  # table input method support
+       fcitx5-nord            # a color theme
+     ];
+   };
   };
 
   boot.supportedFilesystems = [ "ntfs" ];
