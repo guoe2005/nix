81b9228cdf48dac05167f12b938c051b51f53f0e
guoe2005 <guoe2005@126.com>
2024-10-10 17:22:56 +0800

    hw

--- flake.nix
+++ flake.nix
@@ -36,6 +36,7 @@
         modules = [
           inputs.stylix.nixosModules.stylix
           ./configuration.nix
+          /home/guoyi/nix/hardware-configuration_surface.nix
           # nixos-hardware.nixosModules.microsoft-surface-pro-intel
           # 将 home-manager 配置为 nixos 的一个 module
           # 这样在 nixos-rebuild switch 时，home-manager 配置也会被自动部署
