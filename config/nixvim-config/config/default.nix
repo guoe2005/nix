85b434b6cbc0874e571834e42b5e6d495a9e9b94
guoe2005 <guoe2005@126.com>
2024-10-17 16:32:36 +0800

    a

--- config/nixvim-config/config/default.nix
+++ config/nixvim-config/config/default.nix
@@ -270,8 +270,71 @@
         }
       }
     }
+require("telescope").load_extension("lazygit")
 
-    '';
+    luasnip = require("luasnip")
+    kind_icons = {
+      Text = "󰊄",
+      Method = "",
+      Function = "󰡱",
+      Constructor = "",
+      Field = "",
+      Variable = "󱀍",
+      Class = "",
+      Interface = "",
+      Module = "󰕳",
+      Property = "",
+      Unit = "",
+      Value = "",
+      Enum = "",
+      Keyword = "",
+      Snippet = "",
+      Color = "",
+      File = "",
+      Reference = "",
+      Folder = "",
+      EnumMember = "",
+      Constant = "",
+      Struct = "",
+      Event = "",
+      Operator = "",
+      TypeParameter = "",
+    } 
+
+    local cmp = require'cmp'
+
+    -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline({'/', "?" }, {
+      sources = {
+        { name = 'buffer' }
+      }
+    })
+
+    -- Set configuration for specific filetype.
+     cmp.setup.filetype('gitcommit', {
+       sources = cmp.config.sources({
+         { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+       }, {
+         { name = 'buffer' },
+       })
+     })
+
+     -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+     cmp.setup.cmdline(':', {
+       sources = cmp.config.sources({
+         { name = 'path' }
+       }, {
+         { name = 'cmdline' }
+       }),
+  --      formatting = {
+  --       format = function(_, vim_item)
+  --         vim_item.kind = cmdIcons[vim_item.kind] or "FOO"
+  --       return vim_item
+  --      end
+  -- }
+       })  '';
+
+
 extraPlugins = with pkgs.vimPlugins;
     [
       vim-be-good
@@ -301,5 +364,16 @@ extraPlugins = with pkgs.vimPlugins;
       #   };
       # })
     ];
+
+
+  extraConfigLuaPre = ''
+    if vim.g.have_nerd_font then
+      require('nvim-web-devicons').setup {}
+    end
+  '';
+
+  extraConfigLuaPost = ''
+    -- vim: ts=2 sts=2 sw=2 et
+  '';
 
 }
